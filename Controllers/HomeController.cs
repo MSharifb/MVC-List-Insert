08dbd9186d40a5ec082baf88fd43f678aebb40b4
MSharifb <96659557+MSharifb@users.noreply.github.com>
2022-09-26 13:05:05 +0600

    all are ok

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {  
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(EmployeeModel model)
+        {
+            EmployeeViewModel model2 = new EmployeeViewModel();
+            model2.Empinfo.Add(model);
+            return View(model2);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeSystem.Models
+{
+    public class EmployeeModel
+    {
+
+        public int Emp_ID { get; set; }
+        public string Emp_Name { get; set; }
+        public string Emp_Father_Name { get; set; }
+        public string Emp_Mother_Name { get; set; }
+        public string Emp_BloodGroup { get; set; }
+        public DateTime Emp_JoinDate { get; set; }
+        public double Emp_Salary { get; set; }
+        public string Emp_PresentAddress { get; set; }
+        public DateTime Emp_DateOfBirth { get; set; }
+        public string Emp_Marit_Status { get; set; }
+        public string Emp_PhoneNumber { get; set; }        
+    }
+    public class EmployeeViewModel {
+       public List<EmployeeModel> Empinfo = new List<EmployeeModel>();
+    }
+}
